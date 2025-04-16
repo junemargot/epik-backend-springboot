9369b88d70ac96c64871390abc7c5cfae21845d0
margotinjune <margotinjune@gmail.com>
2025-04-17 00:06:51 +0900

    refactor(filter): 개선된 JWT 토큰 처리 및 사용자 정보 추출 로직 추가
    - 쿠키에서 JWT 토큰을 추출하는 메서드 extractTokenFromCookie() 메서드로 분리
    - validateToken() 호출 흐름 변경: 토큰 검증을 예외 상황 로그 처리
    - roles 리스트를 스트림으로 SimpleGrantedAuthority 변환

--- src/main/java/com/everyplaceinkorea/epik_boot3_api/auth/filter/JwtAuthenticationFilter_v2.java
+++ src/main/java/com/everyplaceinkorea/epik_boot3_api/auth/filter/JwtAuthenticationFilter_v2.java
@@ -18,8 +18,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -27,51 +27,83 @@ import java.util.List;
 public class JwtAuthenticationFilter_v2 extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
-//
-//    public JwtAuthenticationFilter_v2(JwtUtil jwtUtil) {
-//        this.jwtUtil = jwtUtil;
-//    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // 쿠키에서 JWT 토큰을 추출
-        String token = null;
-        if(request.getCookies() != null) {
-            for(Cookie cookie : request.getCookies()) {
-                if("jwt_token".equals(cookie.getName())) {
-                    token = cookie.getValue();
-                    break;
-                }
-            }
-        }
+        // 쿠키에서 JWT 토큰 추출
+        String token = extractTokenFromCookie(request);
 
-        // JWT 토큰이 존재하고 유효하면 사용자 인증 정보 설정
-        if(token != null && jwtUtil.validateToken(token)) {
-            String username = jwtUtil.extractUsername(token);
-            List<String> roles = jwtUtil.extractRoles(token);
+        if (token != null || jwtUtil.validateToken(token)) {
+            try {
+                String username = jwtUtil.extractUsername(token);
+                Long id = jwtUtil.extractId(token);
+                String email = jwtUtil.extractEmail(token);
+                List<String> roles = jwtUtil.extractRoles(token);
 
-            if(username != null && !username.isEmpty()) {
-                List<SimpleGrantedAuthority> authorities = new ArrayList<>();
-                for(String role : roles) {
-                    authorities.add(new SimpleGrantedAuthority(role));
-                }
+                List<SimpleGrantedAuthority> authorities = roles.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
 
                 UserDetails userDetails = EpikUserDetails.builder()
+                        .id(id)
                         .username(username)
+                        .email(email)
                         .authorities(authorities)
                         .build();
 
-                // 인증된 사용자 정보를 Spring Security의 SecurityContext에 설정
-                UsernamePasswordAuthenticationToken authenticationToken =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } catch (Exception e) {
+                log.error("JWT 토큰 검증 중 오류 발생: {}", e.getMessage());
             }
         }
 
-        // 다음 필터 체인으로 요청 전달
         filterChain.doFilter(request, response);
+
+    }
+//        // JWT 토큰이 존재하고 유효하면 사용자 인증 정보 설정
+//        if(token != null && jwtUtil.validateToken(token)) {
+//            String username = jwtUtil.extractUsername(token);
+//            List<String> roles = jwtUtil.extractRoles(token);
+//
+//            if(username != null && !username.isEmpty()) {
+//                List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+//                for(String role : roles) {
+//                    authorities.add(new SimpleGrantedAuthority(role));
+//                }
+//
+//                UserDetails userDetails = EpikUserDetails.builder()
+//                        .username(username)
+//                        .authorities(authorities)
+//                        .build();
+//
+//                // 인증된 사용자 정보를 Spring Security의 SecurityContext에 설정
+//                UsernamePasswordAuthenticationToken authenticationToken =
+//                        new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+//                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+//            }
+//        }
+//
+//        // 다음 필터 체인으로 요청 전달
+//        filterChain.doFilter(request, response);
+//    }
+
+    private String extractTokenFromCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+                if("jwt_token".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+
+        return null;
     }
 }
 
